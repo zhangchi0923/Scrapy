f99fd393afb0781400fb95afb33a34a663b3e15b
zhangchi0923 <1099327299@qq.com>
2019-05-13 14:06:18 +0800

    Update Task1.1.py

--- Task1.1.py
+++ Task1.1.py
@@ -2,17 +2,23 @@ import requests
 
 
 #get请求
-
-headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) '
+hd = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) '
             'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
-try:
-    response = requests.get("http://www.baidu.com/",headers = headers)
-except requests.HTTPError as e:
-    # print(response.text)
-     print(response.status_code)
 
-#post请求
-formdata = {'form_email':'1099327299@qq.com','form_password':'stayin1302@'}
-response = requests.post('http://www.douban.com/login',formdata)
-print (response.text)
+def getHTMLText(url):
+    try:
+        r = requests.get(url,timeout=30)
+        r.raise_for_status()            #如果状态不是200，则引发HTTPError异常
+        r.encoding = r.apparent_encoding
+        return r.text
+    except:
+        return "Exception!"
+
+
+if __name__ == "__main__":
+    url = "http://www.baidu.com"
+    print(getHTMLText(url))
+
+
+
 
